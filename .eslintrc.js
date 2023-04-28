2727e41be8f5d205f1dde6e8f5f597835d246aeb
Andreas Dranidis <adranidis@trigonongroup.com>
2023-04-28 11:09:00 +0300

    Initiate repo

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: ["jest"],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["google", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
