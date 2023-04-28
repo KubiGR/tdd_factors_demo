c36a9d0b16053cf7d62e44d697a97b56d1651a83
Andreas Dranidis <adranidis@trigonongroup.com>
2023-04-28 11:37:21 +0300

    Pass test 4

--- factors.cjs
+++ factors.cjs
@@ -1,5 +1,17 @@
 function getFactors(number) {
-  return [number];
+  const factors = [];
+  if (number % 2 === 0) {
+    factors.push(2);
+    number /= 2;
+
+    if (number > 1) {
+      factors.push(number);
+    }
+  } else {
+    factors.push(number);
+  }
+
+  return factors;
 }
 
 module.exports = {
