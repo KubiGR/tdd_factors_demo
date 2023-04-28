7382d4651b832903761d5c6173b76b6412c97c52
Andreas Dranidis <adranidis@trigonongroup.com>
2023-04-28 11:29:38 +0300

    Add test 2 FAIL

--- factors.test.js
+++ factors.test.js
@@ -1,8 +1,13 @@
 const { getFactors } = require("./factors.cjs");
 
 describe("factors", () => {
-  it("should return 1 when getting factors of 1", () => {
+  it("should return [1] when getting factors of 1", () => {
     const factors = getFactors(1);
     expect(factors).toEqual([1]);
+  });
+
+  it("should return [2] when getting factors of 2", () => {
+    const factors = getFactors(2);
+    expect(factors).toEqual([2]);
   });
 });
