d8d68894787f2dcbb151887234cda606c386b16d
Andreas Dranidis <adranidis@trigonongroup.com>
2023-04-28 11:55:33 +0300

    Refactor with inc divisor

--- factors.cjs
+++ factors.cjs
@@ -1,15 +1,15 @@
 function getFactors(number) {
-  const factors = [];
   if (number === 1) return [1];
 
-  while (number % 2 === 0) {
-    factors.push(2);
-    number /= 2;
-  }
+  const factors = [];
+  let divisor = 2;
 
-  while (number % 3 === 0) {
-    factors.push(3);
-    number /= 3;
+  while (number > 1) {
+    while (number % divisor === 0) {
+      factors.push(divisor);
+      number /= divisor;
+    }
+    divisor++;
   }
 
   if (number > 1) {
